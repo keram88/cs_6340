dfd6951868c79e3331c3c6f5589898708221f70a
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2017-09-14 18:24:40 -0600

    Still broken

--- hw2/gramify.py
+++ hw2/gramify.py
@@ -2,10 +2,19 @@ WS = ('\t', '\n', ' ', '\r', '\v')
 PUNC = ('.', '?', '!')
 
 def to_unigrams(s):
-  for u in s.split():
+  for u in s.strip().split():
     yield u.lower()
       
 
-
 def to_bigrams(s):
-  pass
+  last = None
+  s = s.strip().split()
+  i = 0
+  while i < len(s):
+    u = s[i].lower()
+    yield (last, u)
+    if u in PUNC:
+      last = None
+    else:
+      last = u
+    i += 1
--- hw2/test_unigram.py
+++ hw2/test_unigram.py
@@ -1,17 +1,75 @@
+# /usr/bin/env python3
+
 from gramify import to_unigrams
+from gramify import to_bigrams
 import math
+from pprint import pprint
 
-freqs = dict()
 
-count = 0
+unigram_freqs = dict()
+unigram_count = 0
+
+# Maps w_{n-1} to w_n and ("count")
+bigram_freqs = dict()
+bigrams_seen = 0
+
+def bigram_created():
+    global bigrams_seen
+    bigrams_seen += 1
+    return 0
+
+def bigram_logprob(b):
+    if (b[0] not in bigram_freqs.keys() and
+        b[1] not in bigram_freqs[b[0]].keys()):
+        raise Exception()
+    
+    f = bigram_freqs[b[0]][b[1]]
+    if b[0] is None:
+        den = bigram_freqs[None][("count",)]
+
+    else:
+        den = unigram_freqs[b[0]]       
+    return math.log(f/den,2)
+
+def to_bigram_logprob(s):
+    try:
+        s = math.fsum(map(lambda x: bigram_logprob(x), to_bigrams(s)));
+    except:
+        return "undefined"
+    return "{:0.4f}".format(s)
+
+def unigram_logprob(u):
+    return math.log(unigram_freqs[u]/unigram_count, 2)
+
+def to_unigram_logprob(s):
+    s = math.fsum(map(lambda x: unigram_logprob(x), to_unigrams(s)));
+    return "{:0.4f}".format(s)
 
 with open("train.txt", 'r') as f:
-    count = 0
-    for l in f:
-        for u in to_unigrams(l):
-            freqs[u] = freqs.get(u, 0) + 1
-            count += 1
+    # Unigrams
+    l = f.read()
+    for u in to_unigrams(l):
+        unigram_freqs[u] = unigram_freqs.get(u, 0) + 1
+        unigram_count += 1
+        
+    # Bigrams
+    for u in to_bigrams(l):
+        w_n1, w_n = u
+        if w_n1 not in bigram_freqs.keys():
+            bigram_freqs[w_n1] = {("count",) : 0}
+        bigram_freqs[w_n1][("count",)] += 1
+        bigram_freqs[w_n1][w_n] = bigram_freqs[w_n1].get(w_n, bigram_created()) + 1
 
-p=math.fsum(map(lambda x: math.log(freqs[x.lower()]/count, 2), "But old Mr. Toad will leave one day . ".split()))
-            
-print("{:0.4f}".format(p))
+num_unigrams = len(unigram_freqs.keys())
+potential_bigrams = (num_unigrams+1)*num_unigrams
+with open("test.txt") as f:
+    for s in f:
+        s = s.strip()
+        print('''S = {}
+
+Unsmoothed Unigrams, logprob(S) = {}
+Unsmoothed Bigrams, logprob(S) = {}
+'''.format(s,
+           to_unigram_logprob(s),
+           to_bigram_logprob(s)))
+
