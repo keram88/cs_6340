167f2e260c5fd7043ab969f57e4f00c9e851867c
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2017-10-26 00:00:31 -0600

    To vector

--- program3/ner.py
+++ program3/ner.py
@@ -109,19 +109,24 @@ def parse_file(input):
         word_cons = [None, None]
         pos_cons = [None, None]
         eos = False
+        bio = None
         for l in f:
-            l = l.strip)(
+            l = l.strip()
             l = l.split()
             if len(l) == 0:
                 iters = 0
                 if not eos:
                     yield (word_cons[0], word_cons[1], "OMEGA",
                            pos_cons[0], pos_cons[1], "OMEGAPOS",
-                           is_abbr(word_cons[0]), is_cap(word_cons[0]), is_loc(word_cons[0]))
+                           is_abbr(word_cons[0]), is_cap(word_cons[0]), is_loc(word_cons[0]), bio)
                     eos = True
                 continue
             assert(len(l) == 3)
-            bio, pos, w = l[0], l[1], l[2]
+            nbio, pos, w = l[0], l[1], l[2]
+            if w not in WORDS:
+                w = "UNK"
+            if pos not in POSS:
+                pos = "UNKPOS"
             if iters == 0:
                 word_cons[0] = "PHI"
                 pos_cons[0] = "PHIPOS"
@@ -129,56 +134,37 @@ def parse_file(input):
             if iters > 0:
                 yield (word_cons[0], word_cons[1], w,
                        pos_cons[0], pos_cons[1], pos,
-                       is_abbr(word_cons[0]), is_cap(word_cons[0]), is_loc(word_cons[0]))
+                       is_abbr(word_cons[0]), is_cap(word_cons[0]), is_loc(word_cons[0]), bio)
+            bio = nbio
             iters += 1
             word_cons[1], pos_cons[1] = word_cons[0], pos_cons[0]
             word_cons[0], pos_cons[0] = w, pos
             eos = False
 
+# to_feature_idx(dic, word_list, word, start_idx):
+def produce_vecs(input, opt, word_list, word_dict, pos_list, pos_dict, fout):
+    for i in parse_file(input):
+        result = []
+        w, w_prev, w_next, pos, pos_prev, pos_next, abbr, cap, loc, lab = i
+        if opt.word:
+            result.append(to_feature_idx(word_dict, word_list, w, opt.word_idx))
+        if opt.wordcon:
+            result.append(to_feature_idx(word_dict, word_list, w_next, opt.word_next_idx))
+            result.append(to_feature_idx(word_dict, word_list, w_prev, opt.word_prev_idx))
+        if opt.pos:
+            result.append(to_feature_idx(pos_dict, pos_list, pos, opt.pos_idx))
+        if opt.poscon:
+            result.append(to_feature_idx(pos_dict, pos_list, pos_next, opt.pos_next_idx))
+            result.append(to_feature_idx(pos_dict, pos_list, pos_prev, opt.pos_prev_idx))
+        if opt.abbr and abbr:
+            result.append(opt.abbr_idx)
+        if opt.cap and cap:
+            result.append(opt.cap_idx)
+        if opt.locs and loc:
+            result.append(opt.loc_idx)
+#        print(i)
+        print(str(labelings[lab]) + " " + " ".join(list(map(lambda x: str(x) + ":1", sorted(result)))))
             
-def produce_vecs(input, opt, word_list, word_dict, pos_list, pos_dict, unk):
-    with open(input, 'r') as f:
-        iters = 0
-        word_cons = [None, None]
-        pos_cons = [None, None]
-        eos = False
-        for l in f:
-            l = l.strip()
-            l = l.split()
-            if len(l) == 0:
-                iters = 0
-                if not eos:
-                    print(
-'''WORD: {}:1
-WORDCON: {} {}
-POS: {}
-POSCON: {} {}
-ABBR: {}
-CAP: {}
-LOCATION: {}\n'''.format(to_feature_idx(word_dict, word_list, word_cons[0], 1), word_cons[1], "OMEGA", pos_cons[0], pos_cons[1], "OMEGAPOS",
-                       yes_no(is_abbr(word_cons[0])), yes_no(is_cap(word_cons[0])), yes_no(is_loc(word_cons[0]))))
-                    eos = True
-                continue
-            assert(len(l) == 3)
-            bio, pos, w = l[0], l[1], l[2]
-            if iters == 0:
-                word_cons[0] = "PHI"
-                pos_cons[0] = "PHIPOS"
-            print(word_cons, pos_cons, w, pos)
-            if iters > 0:
-                print(
-'''WORD: {}:1
-WORDCON: {} {}
-POS: {}
-POSCON: {} {}
-ABBR: {}
-CAP: {}
-LOCATION: {}\n'''.format(to_feature_idx(word_dict, word_list, word_cons[0], 1), word_cons[1], w, pos_cons[0], pos_cons[1], pos,
-                       yes_no(is_abbr(word_cons[0])), yes_no(is_cap(word_cons[0])), yes_no(is_loc(word_cons[0]))))
-            iters += 1
-            word_cons[1], pos_cons[1] = word_cons[0], pos_cons[0]
-            word_cons[0], pos_cons[0] = w, pos
-            eos = False
 
 def read_locs(opt):
     with open(opt.locs, 'r') as f:
@@ -190,10 +176,10 @@ def yes_no(b):
         return "yes"
     return "no"
             
-def produce_readable(input, opt, unk):
+def produce_readable(input, opt, fout):
     na = lambda x,y: (x if y else "n/a")
     for i in parse_file(input):
-        w, w_next, w_prev, pos, pos_next, pos_prev, abbr, cap, loc = i
+        w, w_prev, w_next, pos, pos_prev, pos_next, abbr, cap, loc, lab = i
         print(
 '''WORD: {}
 WORDCON: {}
@@ -201,11 +187,11 @@ POS: {}
 POSCON: {}
 ABBR: {}
 CAP: {}
-LOCATION: {}\n'''.format(na(w, opt.word), na(w_next+" "+w_prev, opt.wordcon),
-                         na(pos, opt.pos), na(pos_next+" "+pos_prev, opt.poscon),
+LOCATION: {}\n'''.format(na(w, opt.word), na(w_prev+" "+w_next, opt.wordcon),
+                         na(pos, opt.pos), na(pos_prev+" "+pos_next, opt.poscon),
                          na(yes_no(abbr), opt.abbr),
                          na(yes_no(cap), opt.cap),
-                         na(yes_no(loc), opt.location)))
+                         na(yes_no(loc), opt.location)), file=fout)
             
 def run_ner():
     opt = process_args()
@@ -215,8 +201,20 @@ def run_ner():
     pos_list = list(POSS)
     word_dict = dict()
     pos_dict = dict()
-    produce_readable(opt.train, opt, False)
-    #produce_vecs(opt.train, opt, word_list, word_dict, pos_list, pos_dict, False)
+    
+    opt.word_idx = 1
+    opt.word_next_idx = opt.word_idx + len(word_list) + 3 # OMEGA + UNK
+    opt.word_prev_idx = opt.word_next_idx + len(word_list) + 3 # PHI + UNK
+    opt.pos_idx = opt.word_prev_idx + len(word_list) + 3 # UNK
+    opt.pos_next_idx = opt.pos_idx + len(pos_list) + 3 # UNK + OMEGAPOS
+    opt.pos_prev_idx = opt.pos_next_idx + len(pos_list) + 3 # UNK + PHIPOS
+    opt.abbr_idx = opt.pos_prev_idx + len(pos_list) + 2
+    opt.cap_idx = opt.abbr_idx + 2
+    opt.loc_idx = opt.cap_idx + 2
+    
+    
+    #produce_readable(opt.train, opt, sys.stdout)
+    produce_vecs(opt.train, opt, word_list, word_dict, pos_list, pos_dict, sys.stdout)
     
     
 if __name__ == '__main__':
